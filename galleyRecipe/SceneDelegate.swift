30741c09c5f3ddc06ad8a3d9c6d0c2ca369517e6
rafia <safinarafia@gmail.com>
2023-01-16 18:30:36 +0300

    setup buttons

--- galleyRecipe/SceneDelegate.swift
+++ galleyRecipe/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = FavoritesViewController()
+        window?.rootViewController = TimerViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
